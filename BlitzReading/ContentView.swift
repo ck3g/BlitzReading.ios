e7b9bce6582e2c20d17bd53769625b417840d7f6
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-01-20 09:47:16 +0100

    Build a start practice screen

--- BlitzReading/ContentView.swift
+++ BlitzReading/ContentView.swift
@@ -8,13 +8,48 @@
 
 import SwiftUI
 
+struct PracticeButton: View {
+  let text: String
+  let action: () -> Void
+
+  var body: some View {
+    Button(action: self.action) {
+      Text(self.text)
+    }
+    .padding()
+    .frame(width: 250)
+    .foregroundColor(.blue)
+    .background(Color.white)
+    .overlay(
+      RoundedRectangle(cornerRadius: 10).stroke(Color.blue, lineWidth: 2)
+    )
+    .padding(10)
+  }
+}
+
 struct ContentView: View {
   @State private var locale = "en"
   @State private var words: [String] = []
 
+  let locales = ["en": "English", "de": "German"]
+
   var body: some View {
     VStack {
-      Text("Words: \(words.count)")
+      Text("Practice \(locales[locale] ?? "English") words")
+        .font(.headline)
+        .padding()
+
+      PracticeButton(text: "for 5 seconds", action: {
+
+      })
+
+      PracticeButton(text: "for 30 seconds", action: {
+
+      })
+
+      PracticeButton(text: "for 1 minute", action: {
+
+      })
     }
     .onAppear(perform: {
       self.words = Bundle.main.decode("words.\(self.locale).json")
