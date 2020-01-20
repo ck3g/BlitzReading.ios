bdb452c115ae446f9c620a692aa345c10376f594
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-01-20 08:44:42 +0100

    Initial commit

---
+++ BlitzReading/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BlitzReading
+//
+//  Created by Vitali Tatarintev on 20.01.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
