16a2f456dde92bf2552ea8c82ee0121b3478a835
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-01-21 08:11:46 +0100

    Start a countdown timer when practice is started

--- BlitzReading/PracticeView.swift
+++ BlitzReading/PracticeView.swift
@@ -11,8 +11,23 @@ import SwiftUI
 struct PracticeView: View {
   let durationInSeconds: Int
 
+  let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+  @State private var timeRemaining = 0
+
   var body: some View {
-    Text("Practice started: \(durationInSeconds) seconds")
+    VStack {
+      Text("Practice started: \(durationInSeconds) seconds")
+      Text("Time left: \(timeRemaining)")
+    }
+    .onReceive(timer) { time in
+      if self.timeRemaining > 0 {
+        self.timeRemaining -= 1
+      }
+    }
+    .onAppear(perform: {
+      self.timeRemaining = self.durationInSeconds
+    })
   }
 }
 
