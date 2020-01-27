5193fe917ac9d3c9639da22ef8a048c6c13213d3
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-01-27 09:37:38 +0100

    Add language picket to the Settings view

--- BlitzReading/SettingsView.swift
+++ BlitzReading/SettingsView.swift
@@ -9,18 +9,30 @@
 import SwiftUI
 
 struct SettingsView: View {
+  @State private var locale = "en"
+
+  let locales = ["en", "de"]
+  let localesData = [
+    "en": ["name": "English"],
+    "de": ["name": "Deutsch"]
+  ]
+
   var body: some View {
     NavigationView {
-      List {
-        NavigationLink(destination: Text("Language selection view")) {
-          Text("Language")
-        }
+      Form {
+        List {
+          Picker("Language", selection: $locale) {
+            ForEach(self.localesData.keys, id: \.self) {
+              Text(self.localesData[$0]!["name"]!)
+            }
+          }
 
-        NavigationLink(destination: Text("About the app")) {
-          Text("About")
+          NavigationLink(destination: Text("About the app")) {
+            Text("About")
+          }
         }
       }
-      .navigationBarTitle(Text("Settings"))
+      .navigationBarTitle(Text("Settings"), displayMode: .inline)
     }
   }
 }
