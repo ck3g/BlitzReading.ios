0b941c1fce21b4b487320aa97cf59c7b757bafd9
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-05 04:26:22 +0100

    Change highscores tab icon

--- BlitzReading/ContentView.swift
+++ BlitzReading/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView: View {
 
           HighscoresView()
             .tabItem {
-              Image("trophy-icon")
+              Image(systemName: "rosette")
               Text("Highscores")
             }
 
